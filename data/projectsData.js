8b9a3e4e414ed3aba66b259c4138b08f3b3ca55c
Josh May <joshmay222@gmail.com>
2022-12-09 06:37:16 -0600

    new

---
+++ data/projectsData.js
@@ -0,0 +1,44 @@
+const projectsData = [
+  {
+    title: 'The Joe Rogan Experience',
+    description: `What if you could look up any information in the world? Webpages, images, videos
+    and more. Google has many features to help you find exactly what you're looking
+    for.`,
+    imgSrc: '/static/images/rogan.jpg',
+    href: 'https://www.google.com',
+  },
+  {
+    title: 'Huberman Lab',
+    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
+    to the desired date and press "Go". No more worrying about lost keys or
+    forgotten headphones with this simple yet affordable solution.`,
+    imgSrc: '/static/images/huberman.jpg',
+    href: '/blog/the-time-machine',
+  },
+  {
+    title: 'New Heights with Jason Travis Kelce',
+    description: `What if you could look up any information in the world? Webpages, images, videos
+        and more. Google has many features to help you find exactly what you're looking
+        for.`,
+    imgSrc: '/static/images/new-heights.jpg',
+    href: 'https://www.google.com',
+  },
+  {
+    title: 'Crime Junkie',
+    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
+        to the desired date and press "Go". No more worrying about lost keys or
+        forgotten headphones with this simple yet affordable solution.`,
+    imgSrc: '/static/images/crime-junkie.png',
+    href: '/blog/the-time-machine',
+  },
+  {
+    title: 'Lex Fridman Podcast',
+    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
+        to the desired date and press "Go". No more worrying about lost keys or
+        forgotten headphones with this simple yet affordable solution.`,
+    imgSrc: '/static/images/fridman.jpg',
+    href: '/blog/the-time-machine',
+  },
+]
+
+export default projectsData
