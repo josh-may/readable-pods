4f7948482d7ce6a9c368631c26e33fc06e7ab2ae
Josh May <joshmay222@gmail.com>
2022-12-10 08:58:57 -0600

    hubermanlab description

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,41 +1,31 @@
 const projectsData = [
   {
     title: 'The Joe Rogan Experience',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque varius in ante eu gravida. Nunc aliquam pharetra turpis, non facilisis mi dapibus eget. In hac habitasse platea dictumst. Donec congue, dui eu laoreet ultricies, dolor orci feugiat elit, id interdum quam dolor a massa. Pellentesque a aliquam metus. Quisque posuere dolor velit, sed congue nisl pretium non. Proin non dolor leo. Morbi nec lacus velit.`,
     imgSrc: '/static/images/rogan.jpg',
     href: 'https://www.google.com',
   },
   {
     title: 'Huberman Lab',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
+    description: `"Huberman Lab discusses neuroscience: how our brain and its connections with the organs of our body control our perceptions, our behaviors, and our health. We also discuss existing and emerging tools for measuring and changing how our nervous system works. Dr. Andrew Huberman is a tenured Professor of Neurobiology and Ophthalmology at Stanford School of Medicine...."`,
     imgSrc: '/static/images/huberman.jpg',
-    href: '/blog/the-time-machine',
+    href: 'https://www.hubermanlab.readablepods.com/',
   },
   {
     title: 'New Heights with Jason Travis Kelce',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-        and more. Google has many features to help you find exactly what you're looking
-        for.`,
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque varius in ante eu gravida. Nunc aliquam pharetra turpis, non facilisis mi dapibus eget. In hac habitasse platea dictumst. Donec congue, dui eu laoreet ultricies, dolor orci feugiat elit, id interdum quam dolor a massa. Pellentesque a aliquam metus. Quisque posuere dolor velit, sed congue nisl pretium non. Proin non dolor leo. Morbi nec lacus velit.`,
     imgSrc: '/static/images/new-heights.jpg',
     href: 'https://www.google.com',
   },
   {
     title: 'Crime Junkie',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-        to the desired date and press "Go". No more worrying about lost keys or
-        forgotten headphones with this simple yet affordable solution.`,
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque varius in ante eu gravida. Nunc aliquam pharetra turpis, non facilisis mi dapibus eget. In hac habitasse platea dictumst. Donec congue, dui eu laoreet ultricies, dolor orci feugiat elit, id interdum quam dolor a massa. Pellentesque a aliquam metus. Quisque posuere dolor velit, sed congue nisl pretium non. Proin non dolor leo. Morbi nec lacus velit.`,
     imgSrc: '/static/images/crime-junkie.png',
     href: '/blog/the-time-machine',
   },
   {
     title: 'Lex Fridman Podcast',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-        to the desired date and press "Go". No more worrying about lost keys or
-        forgotten headphones with this simple yet affordable solution.`,
+    description: `Iv`,
     imgSrc: '/static/images/fridman.jpg',
     href: '/blog/the-time-machine',
   },
